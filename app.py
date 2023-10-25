15e04c5db7b86c50e21a8d0b5ec3d540e98bfc60
Srikant Shubham <srikantshubham.personal@gmail.com>
2023-10-25 23:17:39 +0530

    fixed links

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 import csv
 import datetime
+import re
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-
 
 @app.route('/')
 def index():
@@ -14,36 +14,40 @@ def index():
    
     return render_template('index.html', exams=exams)
 
+def generate_slug(name):
+    # Convert to lowercase
+    name = name.lower()
+    # Replace special characters and spaces with hyphens
+    name = re.sub(r'[^a-z0-9]+', '-', name)
+    # Remove leading and trailing hyphens
+    name = name.strip('-')
+    return name
 
 def load_sorted_exams_from_csv(csv_file):
     with open(csv_file, 'r') as file:
         csv_reader = csv.DictReader(file)
         exams = [row for row in csv_reader]
-    
+        for exam in exams:
+            exam['url_exam_name'] = generate_slug(exam['Name of Examination'])
     exams.sort(key=lambda x: datetime.datetime.strptime(x['Date of Commencement of Exam'], '%d.%m.%Y'), reverse=False)
     return exams
-
 
 def countdown_to_date(date_str):
     exam_date = datetime.datetime.strptime(date_str, '%d.%m.%Y')
     current_date = datetime.datetime.now()
     delta = exam_date - current_date
-
     return delta.days
 
-
-@app.route('/exam-detail/<exam_name>')
-def exam_detail(exam_name):
+@app.route('/exam-detail/<exam_name>-<year>')
+def exam_detail(exam_name, year):
     exams = load_sorted_exams_from_csv('./static/assets/dates.csv')
-    exam = next((e for e in exams if e['Name of Examination'] == exam_name), None)
-    
+    exam = next((e for e in exams if e['url_exam_name'] == exam_name and str(e['Year']) == year), None)
+    print("Looking for:", exam_name.replace('-', ' ').title(), year)
     if exam:
         exam['countdown'] = countdown_to_date(exam['Date of Commencement of Exam'])
         return render_template('exam_details.html', exam=exam)
     else:
         return "Exam not found", 404
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
