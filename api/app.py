14f3d0e85754a32a8c5089d6d19b42605d508f0b
Srikant Shubham <srikantshubham.personal@gmail.com>
2023-10-30 13:12:24 +0530

    some other changes

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ import datetime
 import re
 import os
 from fuzzywuzzy import fuzz
-from send_mail import add_exam_request
+from .send_mail import add_exam_request
 from urllib.parse import quote_plus
 from flask_cors import CORS
 
